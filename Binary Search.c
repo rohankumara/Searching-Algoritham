6264ca6a31e455836210e0f49062665bc8acfea7
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2019-10-24 04:17:09 +0530

    Add Comment for the code

--- Binary Search.c
+++ Binary Search.c
@@ -2,23 +2,24 @@
 
 int main()
 {
+ //variable diclaration
     int c, first, last, middle, n, search, array[100];
-
+// Arry size input
     printf("Enter number of elements\n");
     scanf("%d",&n);
-
+// Enter Elements for the array
     printf("Enter %d integers\n", n);
-
+// for loop for iniput data to array
     for (c = 0; c < n; c++)
         scanf("%d",&array[c]);
-
+// input Search key
     printf("Enter value to find\n");
     scanf("%d", &search);
 
     first = 0;
     last = n - 1;
     middle = (first+last)/2;
-
+// impliment the binary Search
     while (first <= last) {
         if (array[middle] < search)
             first = middle + 1;
